de3c7ccddc8da1b22fff7702384cb1e2649e3294
Akhil Chandan <akhil.chandan@innovapost.com>
2025-05-21 15:00:13 -0400

    Adding the create task functionality

--- handlers/tasks.js
+++ handlers/tasks.js
@@ -1,4 +1,8 @@
 const db = require('../db');
+const STATUS = {
+    PENDING: "pending",
+    COMPLETED: "completed"
+}
 
 async function getAllTasks(req, res) {
     try {
@@ -9,6 +13,44 @@ async function getAllTasks(req, res) {
     }
 }
 
+async function createTask(req, res) {
+    const { title, description, status } = req.body;
+
+    if (!title) {
+        return res.status(400).json({ error: 'Title is required' });
+    }
+
+    //do a check on the status field. The only values allowed are 'pending','completed'
+    if (status && ![STATUS.PENDING, STATUS.COMPLETED].includes(status)) {
+        //return an error if the field is something else.
+        return res.status(400).json({ error: 'Status must be "pending" or "completed"' });
+    }
+
+    const timestamp = new Date().toISOString();
+    const task = {
+        title,
+        description: description || '',
+        //set the default to 'pending' if value does not exist or is empty
+        status: status || STATUS.PENDING,
+        createdAt: timestamp,
+        updatedAt: timestamp
+    };
+
+    try {
+        const [id] = await db('tasks').insert(task);
+        const createdTask = await db('tasks').where({ id }).first();
+        res.status(201).json(createdTask);
+    } catch (error) {
+        console.log('Error message: ', error)
+        if (error.message.includes('SQLITE_CONSTRAINT: UNIQUE')) {
+            return res.status(409).json({ error: 'Task with this title already exists' });
+        }
+        res.status(500).json({ error: 'Task creation failed' });
+    }
+}
+
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask
 };
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+//List all of the supported APIs. 
 app.get('/tasks/getAll', tasksHandler.getAllTasks);
+app.post('/tasks', tasksHandler.createTask);
 
 // Centralized Error Handling (Example)
 app.use((err, req, res, next) => {
